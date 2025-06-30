b5285ac80b62a2ce9fd84bd3c56b19142372ae4a
itschrisjang <qiiopasd1@gmail.com>
2025-06-30 09:39:57 +0900

    initial commit

---
+++ limber/src/main/java/club/cmc/limber/LimberApplication.java
@@ -0,0 +1,13 @@
+package club.cmc.limber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimberApplication.class, args);
+	}
+
+}
