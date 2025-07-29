4181ec7c4156b48e28b041b0b17b3b663ff6f628
itschrisjang <qiiopasd1@gmail.com>
2025-07-29 17:56:05 +0900

    refactor: 필요없는 import 제거
    - 필요없는 import 제거

--- oauth/src/main/java/club/cmc/limber/oauth/api/AuthController.java
+++ oauth/src/main/java/club/cmc/limber/oauth/api/AuthController.java
@@ -1,12 +1,13 @@
 package club.cmc.limber.oauth.api;
 
-import club.cmc.limber.oauth.application.OAuthLoginService;
 import club.cmc.limber.oauth.application.TokenService;
-import club.cmc.limber.oauth.domain.OAuthUserInfo;
 import club.cmc.limber.oauth.domain.TokenPair;
 import club.cmc.limber.oauth.dto.TokenRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/users")
--- oauth/src/main/java/club/cmc/limber/oauth/api/OAuthLoginController.java
+++ oauth/src/main/java/club/cmc/limber/oauth/api/OAuthLoginController.java
@@ -1,12 +1,12 @@
 package club.cmc.limber.oauth.api;
 
 import club.cmc.limber.oauth.application.OAuthLoginService;
-import club.cmc.limber.oauth.application.TokenService;
 import club.cmc.limber.oauth.domain.OAuthUserInfo;
-import club.cmc.limber.oauth.domain.TokenPair;
-import club.cmc.limber.oauth.dto.TokenRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/users")
