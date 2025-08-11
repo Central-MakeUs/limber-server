5971d09c5d81f5ea7261e02081a413efd95e7141
Chris Jang <qiiopasd1@gmail.com>
2025-08-11 23:31:36 +0900

    feat: 초기 데이터 저장 INSERT 추가
    - 초기 데이터 저장 INSERT 추가

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -25,23 +25,3 @@ $$;
 -- DB에 권한 부여
 GRANT ALL PRIVILEGES ON DATABASE limberdb TO limber;
 
--- limberdb 데이터베이스 사용
-\c limberdb;
-
--- FOCUS_TYPE 테이블에 데이터 삽입
-DO
-$$
-BEGIN
-   IF NOT EXISTS (
-       SELECT 1 FROM FOCUS_TYPE WHERE title = '학습'
-   ) THEN
-       INSERT INTO FOCUS_TYPE (title, user_id, default_flag, del_flag, sequence, reg_dt, reg_id)
-       VALUES
-           ('학습', NULL, 'Y', 'N', 1, CURRENT_TIMESTAMP, 'admin'),
-           ('업무', NULL, 'Y', 'N', 2, CURRENT_TIMESTAMP, 'admin'),
-           ('회의', NULL, 'Y', 'N', 3, CURRENT_TIMESTAMP, 'admin'),
-           ('작업', NULL, 'Y', 'N', 4, CURRENT_TIMESTAMP, 'admin'),
-           ('독서', NULL, 'Y', 'N', 5, CURRENT_TIMESTAMP, 'admin');
-END IF;
-END
-$$;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO FOCUS_TYPE (title, user_id, default_flag, del_flag, sequence, reg_dt, reg_id)
+VALUES
+    ('학습', NULL, 'Y', 'N', 1, CURRENT_TIMESTAMP, 'admin'),
+    ('업무', NULL, 'Y', 'N', 2, CURRENT_TIMESTAMP, 'admin'),
+    ('회의', NULL, 'Y', 'N', 3, CURRENT_TIMESTAMP, 'admin'),
+    ('작업', NULL, 'Y', 'N', 4, CURRENT_TIMESTAMP, 'admin'),
+    ('독서', NULL, 'Y', 'N', 5, CURRENT_TIMESTAMP, 'admin');
