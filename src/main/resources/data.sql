9b9c04f5cbad460cab007fdc70679b035b2163a4
Chris Jang <qiiopasd1@gmail.com>
2025-08-11 23:47:21 +0900

    feat: 집중유형 기본형 데이터 user_id 추가
    - 집중유형 기본형 데이터 user_id 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO FOCUS_TYPE (title, user_id, default_flag, del_flag, sequence, reg_dt, reg_id)
 VALUES
-    ('학습', NULL, 'Y', 'N', 1, CURRENT_TIMESTAMP, 'admin'),
-    ('업무', NULL, 'Y', 'N', 2, CURRENT_TIMESTAMP, 'admin'),
-    ('회의', NULL, 'Y', 'N', 3, CURRENT_TIMESTAMP, 'admin'),
-    ('작업', NULL, 'Y', 'N', 4, CURRENT_TIMESTAMP, 'admin'),
-    ('독서', NULL, 'Y', 'N', 5, CURRENT_TIMESTAMP, 'admin');
+    ('학습', 'admin', 'Y', 'N', 1, CURRENT_TIMESTAMP, 'admin'),
+    ('업무', 'admin', 'Y', 'N', 2, CURRENT_TIMESTAMP, 'admin'),
+    ('회의', 'admin', 'Y', 'N', 3, CURRENT_TIMESTAMP, 'admin'),
+    ('작업', 'admin', 'Y', 'N', 4, CURRENT_TIMESTAMP, 'admin'),
+    ('독서', 'admin', 'Y', 'N', 5, CURRENT_TIMESTAMP, 'admin');
