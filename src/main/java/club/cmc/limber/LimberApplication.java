ea95979db5b09129dfc2ae8ba73f4aeb7e9f2a5e
itschrisjang <qiiopasd1@gmail.com>
2025-08-14 17:49:03 +0900

    feat: 스케줄링 활성화
    - 스케줄링 활성화

--- src/main/java/club/cmc/limber/LimberApplication.java
+++ src/main/java/club/cmc/limber/LimberApplication.java
@@ -2,8 +2,10 @@ package club.cmc.limber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LimberApplication {
 
 	public static void main(String[] args) {
