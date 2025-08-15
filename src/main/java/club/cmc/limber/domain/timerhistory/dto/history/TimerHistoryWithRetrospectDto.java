bb7538a1d041403df959494abd5d4927c7464632
Chris Jang <qiiopasd1@gmail.com>
2025-08-16 01:36:29 +0900

    fix: enum 선언 누락
    - enum 선언 누락

--- src/main/java/club/cmc/limber/domain/timerhistory/dto/history/TimerHistoryWithRetrospectDto.java
+++ src/main/java/club/cmc/limber/domain/timerhistory/dto/history/TimerHistoryWithRetrospectDto.java
@@ -1,6 +1,7 @@
 package club.cmc.limber.domain.timerhistory.dto.history;
 
 import club.cmc.limber.domain.timer.enums.RepeatCycleCode;
+import club.cmc.limber.domain.timerhistory.enums.FailReason;
 import club.cmc.limber.domain.timerhistory.enums.HistoryStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -19,7 +20,7 @@ public record TimerHistoryWithRetrospectDto(
         String repeatDays,
         LocalDateTime historyDt,
         HistoryStatus historyStatus,
-        String failReason,
+        FailReason failReason,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
         @DateTimeFormat(pattern = "HH:mm")
         @Schema(type = "string", example = "19:00")
